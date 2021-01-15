b31825db783257911efde3d25b4caaaaaf82fc78
mithsin <hsinbluemoon@gmail.com>
2021-01-15 17:01:11 -0500

    update

---
+++ src/themeColor.js
@@ -0,0 +1,8 @@
+export const theme = {
+    btnBuy: '#99cc99',
+    btnSuccess: '#4a90e2',
+    btnRed: '#c00000',
+    btnInfo: '#b3b3b3',
+    textPrime: '#212121',
+    textSecondary: '#828282',
+}
